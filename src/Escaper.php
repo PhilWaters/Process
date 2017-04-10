55824d15a382b70c852cce9d2c8a49604dcb41ae
Phil Waters <phil@waters.codes>
2017-04-10 12:37:27 +0100

    Remove * special case from Escaper::argument

--- src/Escaper.php
+++ src/Escaper.php
@@ -35,6 +35,6 @@ class Escaper
      */
     public function argument($argument)
     {
-        return str_replace("*", "'*'", escapeshellarg($argument));
+        return escapeshellarg($argument);
     }
 }
